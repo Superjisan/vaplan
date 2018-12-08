5b9e49035a9359b5f9f2851cd85c2b876fee6ab4
Jisan Zaman <jzaman@wesleyan.edu>
2018-12-08 11:12:16 -0500

    got first file json to create

--- server/controllers/bills.controller.js
+++ server/controllers/bills.controller.js
@@ -1,11 +1,14 @@
 const osmosis = require('osmosis');
 const _ = require('lodash');
+const fs = require('fs');
 
-const session2018URL = `http://lis.virginia.gov/cgi-bin/legp604.exe?191+lst+ALL`;
+const websiteUrl = `http://lis.virginia.gov/cgi-bin/`;
+const session2019Url = `legp604.exe?191+lst+ALL`;
 const bills = {}
+let nextLink = ``
 
 osmosis
-    .get(session2018URL)
+    .get(`${websiteUrl}${session2019Url}`)
     .set(
         {
             names: ['ul.linkSect:first-of-type li'],
@@ -17,29 +20,49 @@ osmosis
     .follow('@href')
     .set({
         fullName: 'h3.topLine',
-        history: ['#mainC h4:last-of-type + ul.linkSect li']
+        history: ['#mainC h4:last-of-type + ul.linkSect li'],
+        link: 'ul:nth-of-type(2) li a[href]'
     })
     .data((bill) => {
         // do something with listing data
-        // console.log("bill", bill.history);
         if (!_.isEmpty(bill.history)) {
-            // console.log("bill history", bill.history, bill.fullName);
+            // check if subcommittee is there to check
+            const historyItems = _.map(bill.history, historyString => {
+                const historyItem = {
+                    text: historyString
+                };
+                historyItem.date = historyString.split(" ")[0];
+
+                if(historyString.indexOf("Referred to Committee") !== -1) {
+                    bill.committeeText = historyString
+                }
+                if(historyString.indexOf("Assigned") !== -1 && historyString.indexOf("Subcommittee") !== -1) {
+                    bill.subcommitteeText = historyString
+                }
+                return historyItem
+            })
             if (!_.isEmpty(bills)) {
-                // console.log("bill key 1", bills[Object.keys(bills)[0]])
                 const foundBill = _.find(bills, dataBill => {
-                    // console.log("dataBill", dataBill);
                     return bill.fullName.replace(/\s+/, "") === dataBill.name.replace(/\s+/, "")
                 })
                 if (foundBill) {
-                    foundBill.history = bill.history;
-                    // console.log("foundBill", foundBill);
+                    foundBill.history = historyItems;
+                    if(bill.committeeText) {
+                        foundBill.committeeText = bill.committeeText;
+                    }
+                    if(bill.subcommitteeText) {
+                        foundBill.subcommitteeText = bill.subcommitteeText
+                    }
                 }
             }
         }
+        if(bill.link) {
+            nextLink = bill.link;
+        }
+
         if (bill.numbers) {
             // console.log("names", bill.names)
             if (Object.keys(bills).length < bill.numbers.length) {
-                // console.log("putting in data")
                 bill.numbers.forEach((number, index) => {
                     bills[number] = {
                         number,
@@ -57,5 +80,19 @@ osmosis
     })
     // .debug(console.log)
     .done(data => {
-        console.log('bills', Object.keys(bills).length, _.map(bills, 'history'));
+        const dataToSave = {
+            bills,
+            nextLink
+        }
+    
+        const JSONToSave = JSON.stringify(dataToSave);
+        const fileName = `first_crawler_bill.json`;
+        fs.writeFile(`./server/${fileName}`, JSONToSave, (err) => {
+            if(err) {
+                console.error(`something went wrong`, err)
+            } else {
+                console.log(`wrote file ${fileName}`)
+            }
+        })
+
     })
